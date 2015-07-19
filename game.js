2005b217368ad3705e0a9433c55c98e132f9c395
MLee21 <minnie.lee89@gmail.com>
2015-07-19 15:13:06 -0600

    add background and character images.

--- game.js
+++ game.js
@@ -1,5 +1,40 @@
+//Create Canvas
+
 var canvas = document.createElement("canvas");
 var context = canvas.getContext("2d");
 canvas.width = 512;
 canvas.height = 480;
 document.body.appendChild(canvas);
+
+// Background Image
+
+var bgReady = false;
+var bgImage =  new Image();
+
+bgImage.onload = function () {
+  bgReady = true;
+};
+
+bgImage.src = "images/background.png"
+
+// Ash Image
+
+var bgReady = false;
+var bgImage =  new Image();
+
+bgImage.onload = function () {
+  bgReady = true;
+};
+
+bgImage.src = "images/Ash.png"
+
+// Pikachu Image
+
+var bgReady = false;
+var bgImage =  new Image();
+
+bgImage.onload = function () {
+  bgReady = true;
+};
+
+bgImage.src = "images/PikachuSilver.png"
