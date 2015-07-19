a700572b02e21ae4c07a7884b358334cf6903d19
MLee21 <minnie.lee89@gmail.com>
2015-07-19 15:39:12 -0600

    add main game loop.

--- game.js
+++ game.js
@@ -126,3 +126,17 @@ var update = function (modifier) {
   context.textBaseline = "top";
   context.fillText("Monsterrs caught: " + monstersCaught, 32, 32);
 };
+
+// The main game loop
+var main = function () {
+  var now = Date.now();
+  var delta = now - then;
+
+  update(delta / 1000);
+  render();
+
+  then = now;
+
+  // Request to do this again ASAP
+  requestAnimationFrame(main);
+};
