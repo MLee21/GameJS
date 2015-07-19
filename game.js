69cf059102252dcf0cc2cefaf820d5a413855f22
MLee21 <minnie.lee89@gmail.com>
2015-07-19 15:31:30 -0600

    update objects.

--- game.js
+++ game.js
@@ -76,3 +76,31 @@ var reset = function () {
   monster.x = 32 + (Math.random() * (canvas.width - 64));
   monster.y = 32 + (Math.random() * (canvas.height - 64));
 };
+
+
+// Update objects
+
+var update = function (modifier) {
+  if (38 in keysDown) {
+    hero.y -= hero.speed * modifier;
+  }
+  if (40 in keysDown) {
+    hero.y += hero.speed * modifier;
+  }
+  if (37 in keysDown) {
+    hero.x -= hero.speed * modifier;
+  }
+  if (39 in keysDown) {
+    hero.x += hero.speed * modifier;
+  }
+
+  if (
+    hero.x <= (monster.x + 32)
+    && monster.x <= (hero.x + 32
+    && hero.y <= (monster.y + 32)
+    && monster.y <= (hero.y + 32)
+    ) {
+      ++ monsterCaught;
+      reset();
+    }
+};
